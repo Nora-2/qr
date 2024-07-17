e83f86618912a0d43340633eaa7919df86403a02
Your Name <hagaryusuf@gmail.com>
2024-07-18 00:23:02 +0300

    commit newww

--- lib/features/viewdata/presentation/view/viewdata.dart
+++ lib/features/viewdata/presentation/view/viewdata.dart
@@ -1,4 +1,4 @@
-
+//new update
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:awesome_dialog/awesome_dialog.dart';
@@ -21,15 +21,15 @@ class _ViewDataScreenState extends State<ViewDataScreen> {
   late DatabaseHelper _dbHelper;
   final TextEditingController _searchControllerID = TextEditingController();
   final TextEditingController _searchControllerQR = TextEditingController();
-  final TextEditingController _searchControllerDatetime = TextEditingController();
+  final TextEditingController _searchControllerDatetime =
+      TextEditingController();
   String _searchQuery = '';
-
 
   @override
   void initState() {
     super.initState();
     _dbHelper = DatabaseHelper();
-   // _loadData();
+    // _loadData();
   }
 
   Future<void> _loadDataID(String id) async {
@@ -45,7 +45,7 @@ class _ViewDataScreenState extends State<ViewDataScreen> {
     });
   }
 
-   Future<void> _loadDataQR(String qr) async {
+  Future<void> _loadDataQR(String qr) async {
     await _dbHelper.initDatabase();
     List<Map<String, dynamic>> qrcodes;
     if (qr.isEmpty) {
@@ -120,120 +120,120 @@ class _ViewDataScreenState extends State<ViewDataScreen> {
         body: Column(
           children: [
             Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchControllerID,
-              decoration: InputDecoration(
-                labelText: 'Search by ID',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      _searchQuery = _searchControllerID.text;
-                      _loadDataID(_searchQuery);
-                    });
-                  },
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchControllerQR,
-              decoration: InputDecoration(
-                labelText: 'Search by QR',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      _searchQuery = _searchControllerQR.text;
-                      _loadDataQR(_searchQuery);
-                    });
-                  },
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchControllerDatetime,
-              decoration: InputDecoration(
-                labelText: 'Search by Datetime',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      _searchQuery = _searchControllerDatetime.text;
-                      _loadDataDatetime(_searchQuery);
-                    });
-                  },
-                ),
-              ),
-            ),
-          ),
-            
-            
-             _qrcodes.isEmpty
-            ? const Center(child: Text('No data found'))
-            : SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: FittedBox(
-                  fit: BoxFit.scaleDown,
-                  child: DataTable(
-                    columns: const [
-                      DataColumn(label: Text('ID')),
-                      DataColumn(label: Text('Barcode \n الباركود')),
-                      DataColumn(label: Text('DateTime \n التاريخ')),
-                      DataColumn(label: Text('Actions')),
-                    ],
-                    rows: _qrcodes.map<DataRow>((code) {
-                      return DataRow(
-                        cells: [
-                          DataCell(Text('${code['id']}')),
-                          // ignore: sized_box_for_whitespace
-                          DataCell(Container(
-                              width: 100, child: Text('${code['qrCode']}'))),
-                          DataCell(Text('${code['datetime']}')),
-                          DataCell(
-                            IconButton(
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.red,
-                              ),
-                              onPressed: () async {
-                                _deleteQRCode(context, code['id'],
-                                    _qrcodes.indexOf(code));
-                                await Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        const ViewDataScreen(),
-                                  ),
-                                );
-                                customAwesomeDialog(
-                                        context: context,
-                                        dialogType: DialogType.success,
-                                        title: 'Success',
-                                        onOkPressed: () {
-                                          Navigator.pop(context);
-                                        },
-                                        description:
-                                            'The Barcode deleted successfully! \n تم حذف هذا الباركود بنجاح',
-                                        buttonColor: const Color(0xff00CA71))
-                                    .show();
-                              },
-                            ),
-                          ),
-                        ],
-                      );
-                    }).toList(),
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchControllerID,
+                decoration: InputDecoration(
+                  labelText: 'Search by ID',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        _searchQuery = _searchControllerID.text;
+                        _loadDataID(_searchQuery);
+                      });
+                    },
                   ),
                 ),
+              ),
             ),
-        ],
-              ));
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchControllerQR,
+                decoration: InputDecoration(
+                  labelText: 'Search by QR',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        _searchQuery = _searchControllerQR.text;
+                        _loadDataQR(_searchQuery);
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchControllerDatetime,
+                decoration: InputDecoration(
+                  labelText: 'Search by Datetime',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        _searchQuery = _searchControllerDatetime.text;
+                        _loadDataDatetime(_searchQuery);
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            _qrcodes.isEmpty
+                ? const Center(child: Text('No data found'))
+                : SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: DataTable(
+                        columns: const [
+                          DataColumn(label: Text('ID')),
+                          DataColumn(label: Text('Barcode \n الباركود')),
+                          DataColumn(label: Text('DateTime \n التاريخ')),
+                          DataColumn(label: Text('Actions')),
+                        ],
+                        rows: _qrcodes.map<DataRow>((code) {
+                          return DataRow(
+                            cells: [
+                              DataCell(Text('${code['id']}')),
+                              // ignore: sized_box_for_whitespace
+                              DataCell(Container(
+                                  width: 100,
+                                  child: Text('${code['qrCode']}'))),
+                              DataCell(Text('${code['datetime']}')),
+                              DataCell(
+                                IconButton(
+                                  icon: const Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                  onPressed: () async {
+                                    _deleteQRCode(context, code['id'],
+                                        _qrcodes.indexOf(code));
+                                    await Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ViewDataScreen(),
+                                      ),
+                                    );
+                                    customAwesomeDialog(
+                                            context: context,
+                                            dialogType: DialogType.success,
+                                            title: 'Success',
+                                            onOkPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            description:
+                                                'The Barcode deleted successfully! \n تم حذف هذا الباركود بنجاح',
+                                            buttonColor:
+                                                const Color(0xff00CA71))
+                                        .show();
+                                  },
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+          ],
+        ));
   }
 
   Future<void> _deleteAllQRCodes(BuildContext context) async {
@@ -258,4 +258,3 @@ class _ViewDataScreenState extends State<ViewDataScreen> {
     _loadDataID(''); // Wait for data to update
   }
 }
-
